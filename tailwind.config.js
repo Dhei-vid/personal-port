55f360b7ac7e4e934a659fe108641fed294abdd5
David <djetuk@gmail.com>
2021-11-17 15:33:35 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'body': '#17171F',
+        'selected-text': '#A3A3FF',
+        'theme': '#3F3FFF',
+        'nav': '#404053',
+        'secondary': '#9191A4',
+        'badge': '#2424d6',
+        'input-border': '#565666',
+        'input': '#2A2A35'
+      },
+
+      fontFamily: {
+        'poppins': ["Poppins", "sans-serif"]
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+
+}
